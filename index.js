aae96fdd609c67bb51eff8ffc5c299db89d968ad
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-19 16:48:55 +0300

    "Nodemon"

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => res.end("Hello express!"));
+app.use((req, res) => res.end("Hello express!!!"));
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
