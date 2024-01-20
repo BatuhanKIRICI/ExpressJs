a08752feb1465555739abbc33ccd6e994732cab9
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-20 09:14:27 +0300

    "HTML pages"

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.use("/products/:id/users/:username", (req, res) => {
-  console.log(req.params.username);
-  res.send("Products detail");
+const path = require("path");
+
+app.use("/blogs/:blogid", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/users", "blogs-details.html"));
 });
 
-app.use("/products", (req, res) => {
-  res.send("Products");
+app.use("/blogs", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/users", "blogs.html"));
 });
 
 app.use("/", (req, res) => {
-  res.send("Home page");
+  res.sendFile(path.join(__dirname, "views/users", "index.html"));
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
