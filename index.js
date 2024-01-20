552dae95bdd8b95013d378abbbc40917b56b84bb
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-20 08:25:38 +0300

    "Middleware"

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => res.end("Hello express!!!"));
+app.use((req, res, next) => {
+  console.log("middleware 1");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("middleware 2");
+  next();
+});
+
+app.use((req, res) => {
+  console.log("middleware 3");
+  res.send("<h1>Home Page</h1>");
+});
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
