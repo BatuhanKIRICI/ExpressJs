bd5e551df9207386a2408c3dce62d7fc1a3532c7
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-20 08:36:12 +0300

    "Routing"

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("middleware 1");
-  next();
+app.use("/products/:id/users/:username", (req, res) => {
+  console.log(req.params.username);
+  res.send("Products detail");
 });
 
-app.use((req, res, next) => {
-  console.log("middleware 2");
-  next();
+app.use("/products", (req, res) => {
+  res.send("Products");
 });
 
-app.use((req, res) => {
-  console.log("middleware 3");
-  res.send("<h1>Home Page</h1>");
+app.use("/", (req, res) => {
+  res.send("Home page");
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
